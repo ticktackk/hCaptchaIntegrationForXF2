356413e31623e6a115742a84b8a4825340273b13
TickTackk <me@ticktackk.io>
2020-04-16 00:32:45 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+hCaptcha Integration for XenForo 2.1.0+
+=======================================
+
+Description
+-----------
+
+This add-on adds allows using hCaptcha throughout XenForo.
+
+Requirements
+------------
+
+- PHP 7.3.0+
+
+License
+-------
+
+This project is licensed under the MIT License - see the [LICENSE.md](https://github.com/ticktackk/hCaptchaIntegrationForXF2/blob/master/LICENSE.md) file for details.
---
+++ Setup.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace TickTackk\hCaptchaIntegration;
+
+use XF\AddOn\AbstractSetup;
+use XF\AddOn\StepRunnerInstallTrait;
+use XF\AddOn\StepRunnerUninstallTrait;
+use XF\AddOn\StepRunnerUpgradeTrait;
+use XF\Db\Schema\Alter as AlterDbSchema;
+
+/**
+ * Class Setup
+ *
+ * @package TickTackk\hCaptchaIntegration
+ */
+class Setup extends AbstractSetup
+{
+	use StepRunnerInstallTrait;
+	use StepRunnerUpgradeTrait;
+	use StepRunnerUninstallTrait;
+}
